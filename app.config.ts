f04b8283bc32eb81a23d20ddfaab3b58b1968a2c
Jhonatan Villanueva <jinvillaz@gmail.com>
2024-10-27 13:18:49 -0400

    feat: fix plugins app config and handle change theme by system

--- app.config.ts
+++ app.config.ts
@@ -33,7 +33,15 @@ const config: ExpoConfig = {
   },
   plugins: [
     'expo-router',
-    ['expo-localization', { ios: { useFrameworks: 'static' } }],
+    'expo-localization',
+    [
+      'expo-build-properties',
+      {
+        ios: {
+          useFrameworks: 'static',
+        },
+      },
+    ],
     '@react-native-firebase/app',
     '@react-native-firebase/auth',
     '@react-native-google-signin/google-signin',
@@ -41,17 +49,6 @@ const config: ExpoConfig = {
   experiments: {
     typedRoutes: true,
   },
-  /* extra: {
-    firebaseApiKey: process.env.FIREBASE_API_KEY,
-    firebaseAuthDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    firebaseDatabaseUrl: process.env.FIREBASE_DATABASE_URL,
-    firebaseProjectId: process.env.FIREBASE_PROJECT_ID,
-    firebaseStorageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    firebaseMessagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    firebaseAppId: process.env.FIREBASE_APP_ID,
-    firebaseMeasurementId: process.env.FIREBASE_MEASUREMENT_ID,
-    androidClientId: process.env.ANDROID_CLIENT_ID,
-  }, */
 }
 
 export default config
