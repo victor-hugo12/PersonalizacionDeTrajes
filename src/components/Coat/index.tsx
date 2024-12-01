155fedfdff523611d32b1eedbf75197cfd6547a5
Jhonatan Villanueva <jinvillaz@gmail.com>
2024-11-30 20:08:35 -0400

    Resolve issue ignore case letters

---
+++ src/components/Coat/index.tsx
@@ -0,0 +1,61 @@
+import React from 'react'
+import { StyleSheet, View } from 'react-native'
+import Svg, { Circle, Path } from 'react-native-svg'
+
+import { WHITE } from '@/constants/colors'
+import { calculateCoat } from '@/utils/calculateCoat'
+
+interface CoatProps {
+  width?: number
+  height?: number
+  length: number
+  shoulder: number
+  chest: number
+  arm: number
+  fillColor?: string
+  strokeColor?: string
+}
+
+export const Coat: React.FC<CoatProps> = ({
+  width = 400,
+  height = 400,
+  length,
+  shoulder,
+  chest,
+  arm,
+  fillColor = 'gray',
+  strokeColor = 'black',
+}) => {
+  const { original, reflected, pocket, mirroredPocket, buttons, neck, neck1, neck1Mirrored, arm1, arm1Mirrored } =
+    calculateCoat({ length, shoulder, chest, arm }, width, height)
+
+  return (
+    <View style={styles.imageWrapper}>
+      <Svg width={300} height={300} viewBox={`0 0 ${400} ${350}`}>
+        <Path d={arm1} fill={fillColor} stroke={strokeColor} strokeWidth="2" />
+        <Path d={arm1Mirrored} fill={fillColor} stroke={strokeColor} strokeWidth="2" />
+        <Path d={neck} fill={fillColor} stroke={strokeColor} strokeWidth="2" />
+        <Path d={reflected} fill={fillColor} stroke={strokeColor} strokeWidth="2" />
+        <Path d={neck1Mirrored} fill={fillColor} stroke={strokeColor} strokeWidth="2" />
+        <Path d={original} fill={fillColor} stroke={strokeColor} strokeWidth="2" />
+        <Path d={neck1} fill={fillColor} stroke={strokeColor} strokeWidth="2" />
+        <Path d={pocket} fill={fillColor} stroke={strokeColor} strokeWidth="2" />
+        <Path d={mirroredPocket} fill={fillColor} stroke={strokeColor} strokeWidth="2" />
+
+        {buttons.map((button, index) => (
+          <Circle key={index} cx={button.cx} cy={button.cy} r={button.r} fill={strokeColor} />
+        ))}
+      </Svg>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  imageWrapper: {
+    flex: 1,
+    width: 300,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: WHITE,
+  },
+})
